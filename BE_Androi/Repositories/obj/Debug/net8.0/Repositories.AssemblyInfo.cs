a0b9de7fd76c825170be806c241fa443cc355fcb
dotrong-code <dotrong159357@gmail.com>
2025-03-24 13:21:25 +0700

    updfastee

--- BE_Androi/Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
+++ BE_Androi/Repositories/obj/Debug/net8.0/Repositories.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dc78bc60e7ce12fff61be5694becdeb4d26357e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+821fcf2f7b45d5c0177bc9829262a083564f8ec8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- BE_Androi/Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
+++ BE_Androi/Services/obj/Debug/net8.0/Services.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Services")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dc78bc60e7ce12fff61be5694becdeb4d26357e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+821fcf2f7b45d5c0177bc9829262a083564f8ec8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Services")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Services")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
